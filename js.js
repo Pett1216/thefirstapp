14119b7df868f5b177618ab627a294c733b99f68
Pett1216 <117648722+Pett1216@users.noreply.github.com>
2022-11-17 13:45:32 +0100

    PettBranch add JS

---
+++ js.js
@@ -0,0 +1,20 @@
+
+/// variables
+var name = false;  /// need change - need to assign it to valueable
+var name = {}; /// if want to use object variable
+var name = []; /// if want to use arr variable
+let arr = [];
+let bool = true;
+let boolf = false;
+let age = 24; /// variable 'age' has tag of a number
+const year = '2022' /// string
+
+/// Functions
+function myf1(){};
+myf1(); /// exclude the function?
+
+/// Statements -- what you need from the browser
+console.log (name)
+let res = 78 + age;
+myf1(); ///-- to run function example
+
